81aae0abded1b5df374985979ca8fbe41decd7a3
lderuijter <85785995+lderuijter@users.noreply.github.com>
2024-06-26 22:14:39 +0200

    implemented core ui, navbar, footer, routing and changed project layout, work in progress, css updates on the way

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,74 @@
+import { CNav, CNavItem, CNavLink } from '@coreui/react';
+import React from "react";
 import './App.css';
 import Projecten from './projecten';
-import Navbar from './navbar';
-import Footer from './footer';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Outlet,
+} from "react-router-dom";
+import { CFooter } from '@coreui/react';
 
 const portfolioEigenaar = {
   naam: 'Lucas de Ruijter',
 }
 
-function WelkomBericht() {
+function Home() {
   return (
     <>
       <h1>Welkom op mijn portfolio!</h1>
-      <p className='markup'>Ik ben {portfolioEigenaar.naam}</p>
+      <p>Ik ben {portfolioEigenaar.naam}</p>
     </>
+  );
+}
+
+function NotFound() {
+  return (
+      <div>
+          <h2>404 Not Found</h2>
+          <p>Deze pagina bestaat niet!</p>
+      </div>
+  );
+}
+
+function Footer() {
+  return (
+    <CFooter>
+  <div className='copyright-contact'>
+    <span>Copyright &copy; 2024 Lucas de Ruijter </span>
+    <a href="mailto:lucasder2003@gmail.com">mail</a>
+  </div>
+  <div className='powered-by'>
+    <span>Powered by CoreUI and React</span>
+  </div>
+</CFooter>
   );
 }
 
 function App() {
   return (
     <>
-      <Navbar />
-      <div className='main'>
-        <WelkomBericht />
-        <Projecten />
+    <Router>
+    <CNav className="flex-column navbar">
+      <CNavItem>
+        <CNavLink className="nav-link">
+          <CNavLink className="nav-link" href="/#">Home</CNavLink>
+        </CNavLink>
+      </CNavItem>
+      <CNavItem>
+        <CNavLink className="nav-link" href="/projecten">Projecten</CNavLink>
+      </CNavItem>
+    </CNav><div className="main">
+        <Routes>
+          <Route path="/" element={<Outlet />}>
+            <Route index element={<Home />} />
+            <Route path="/projecten" element={<Projecten />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
       </div>
+      </Router>
       <Footer />
     </>
   );
